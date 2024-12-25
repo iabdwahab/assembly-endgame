06eed8d9f12401359a607f588ecc0c74941bb377
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-12-25 13:30:07 +0200

    Implementing Eliminated Status.

--- src/components/Elimination.jsx
+++ src/components/Elimination.jsx
@@ -1,6 +1,26 @@
-const Elimination = ({ bgColor, textColor }) => {
+import eliminatedIcon from '../../public/eliminated-icon.png'
+
+const Elimination = ({ data }) => {
+  const { value, bgColor, textColor, isEliminated } = data;
+
+  const styles = {
+    backgroundColor: bgColor,
+    color: textColor
+  }
+
   return (
-    <p className={`bg-[${bgColor}] text-[${textColor}] w-fit px-3 py-1 rounded`}>HTML</p>
+    <div className='relative rounded overflow-hidden'>
+      {isEliminated && <Eliminated />}
+      <p className='font-medium w-fit px-3 py-1' style={styles}>{value}</p>
+    </div >
+  )
+}
+
+function Eliminated() {
+  return (
+    <span className='absolute bg-[#3f3f3fa2] w-full h-full flex justify-center items-center'>
+      <img src={eliminatedIcon} alt="Elimination Icon" className='w-5' />
+    </span>
   )
 }
 
