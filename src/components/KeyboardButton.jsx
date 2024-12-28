51b64aead38729ffe00c4588c3dcd719724883e1
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-12-28 13:57:23 +0200

    Fixing Bug: When clicking the same keybord letter.

--- src/components/KeyboardButton.jsx
+++ src/components/KeyboardButton.jsx
@@ -8,49 +8,51 @@ const KeyboardButton = ({ keyboardLetterObject, setKeyboardLettersState, word, s
         : '#FCBA29'
   }
 
-  function handleClick(clickedLetter) {
+  function handleClick(letterObject) {
+    if (letterObject.status === 'initial') {
 
+      // Count Attempts if the answer was wrong
+      !word.wordText.includes(letterObject.letter) && setAttempts(prevAttempts => prevAttempts + 1);
 
-    // Count Attempts if the answer was wrong
-    !word.wordText.includes(clickedLetter) && setAttempts(prevAttempts => prevAttempts + 1);
+      // Update KEYBOARD State:
+      setKeyboardLettersState(prevState => {
 
-    // Update KEYBOARD State:
-    setKeyboardLettersState(prevState => {
-
-      return prevState.map(prevStateLetter => {
-        if (prevStateLetter.letter === clickedLetter) {
-          return {
-            ...prevStateLetter,
-            status: word.wordText.includes(clickedLetter) ? 'correct' : 'wrong',
-          }
-        } else {
-          return prevStateLetter
-        }
-      })
-
-    })
-
-    // Update Word Viewer
-    setWord(prevWord => {
-      return {
-        wordText: prevWord.wordText,
-        lettersDetails: prevWord.lettersDetails.map(letterDetails => {
-          if (letterDetails.letterText === clickedLetter) {
+        return prevState.map(prevStateLetter => {
+          if (prevStateLetter.letter === letterObject.letter) {
             return {
-              ...letterDetails,
-              isDisplayed: true,
+              ...prevStateLetter,
+              status: word.wordText.includes(letterObject.letter) ? 'correct' : 'wrong',
             }
           } else {
-            return letterDetails
+            return prevStateLetter
           }
         })
-      }
-    })
+
+      })
+
+      // Update Word Viewer
+      setWord(prevWord => {
+        return {
+          wordText: prevWord.wordText,
+          lettersDetails: prevWord.lettersDetails.map(letterDetails => {
+            if (letterDetails.letterText === letterObject.letter) {
+              return {
+                ...letterDetails,
+                isDisplayed: true,
+              }
+            } else {
+              return letterDetails
+            }
+          })
+        }
+      })
+    }
+
 
   }
 
   return (
-    <button ref={buttonRef} className={classNames} onClick={() => handleClick(letter)} style={styles}>{letter}</button>
+    <button ref={buttonRef} className={classNames} onClick={() => handleClick(keyboardLetterObject)} style={styles}>{letter}</button>
   )
 }
 
